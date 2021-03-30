3b2ea42a2cf820522f5068ebe3197aa7852d5022
jasonjiangstuy <jasonjiangny@gmail.com>
2021-03-30 18:10:57 -0400

    created framework and google login

---
+++ mysite/Landing/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+from django.http import HttpResponse
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    current_user = request.user
+    print(current_user.id)
+    for attr, value in vars(current_user).items():
+        print("Attribute: " + str(attr or ""))
+        print("Value: " + str(value or ""))
+    print(current_user._wrapped.username)
+    return render(request, "account/profile.html")
